f05e5fd0d19232a2543308e97393570ff644818f
RahulVirat-18 <rvirat154@gmail.com>
2025-07-14 14:49:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -198,4 +198,4 @@ def open_file(filename):
         return f"Error opening file: {str(e)}", 404
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
